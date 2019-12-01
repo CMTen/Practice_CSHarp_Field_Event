fd3ae26041471cabc8426e39bf95fed94bdb57b6
CMTen <57666274+CMTen@users.noreply.github.com>
2019-12-01 20:02:55 +0800

    Field & Event 作業

---
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 99.9f)]
+    public float speed = 10;
+
+    [Header("跳躍高度"), Range(0, 999)]
+    public int Jump = 200;
+
+    [Header("對話內容")]
+    public string Talk = "咕咕咕";
+
+    [Header("是否取得雞蛋")]
+    public bool Egg = false;
+
+    private void Start()
+    {
+        print("遊戲開始");
+    }
+
+    private void Update()
+    {
+        print("咕咕咕");
+    }
+}
